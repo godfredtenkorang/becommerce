ac0bcc6f50e0cb899e0669f8daf5d37e8ab30efc
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-06-06 10:28:42 +0000

    changes

--- becommerce/settings.py
+++ becommerce/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config(''),
+        'USER': 'godey',
+        'PASSWORD': 'Gony@100',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

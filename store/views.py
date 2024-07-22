953658d14d372043ae6cc92a7386214b138a63cc
Benjamin <benjaminoheneasare65@gmail.com>
2024-07-22 20:05:24 +0000

    changes

--- store/views.py
+++ store/views.py
@@ -10,3 +10,19 @@ def productDetail(request):
 
 def shop(request):
     return render(request, 'store/shop.html')
+
+def cart(request):
+    return render(request, 'store/cart.html')
+
+def category(request):
+    return render(request, 'store/category.html')
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
+
+def myOrder(request):
+    return render(request, 'store/myOrder.html')
+
+
+def WishList(request):
+    return render(request, 'store/wishList.html')

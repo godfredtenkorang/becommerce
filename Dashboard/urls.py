79deb89fe2248ca318b0213c9446238524f82b21
benjamin <benjaminoheneasare65@gmail.com>
2025-04-11 22:56:28 +0000

    changes

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('admin/', views.adminDashbaord, name='admin-dashboard'),
     path('orders/', views.orders, name='orders'),
+    path('sales/', views.sales, name='sales'),
 ]
